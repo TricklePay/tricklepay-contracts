7b42f8fa79a8be49cda77797c2fa2a4d16079ea6
Glittersup <fasholagifty@gmail.com>
2026-06-18 00:15:13 +0100

    refactor: use canonical TokenClient import path

--- contracts/stream/src/contract.rs
+++ contracts/stream/src/contract.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{contract, contractimpl, token::Client as TokenClient, Address, Env};
+use soroban_sdk::{contract, contractimpl, token::TokenClient, Address, Env};
 
 use crate::error::StreamError;
 use crate::events;
